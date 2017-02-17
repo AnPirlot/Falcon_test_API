737ed95123f91db8ba405d125190527d210adc2b
An Pirlot <AnPirlot@gmail.com>
2017-02-17 16:42:31 +0100

    add allowed origins for angular

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import falcon
 
 import tasks.tasks
+import utils
 
-api = application = falcon.API()
+ALLOWED_ORIGINS = utils.conf.allowed_hosts # Or load this from a config file
+
+class CorsMiddleware(object):
+
+    def process_request(self, request, response):
+        origin = request.get_header('Origin')
+        if origin in ALLOWED_ORIGINS:
+            response.set_header('Access-Control-Allow-Origin', origin)
+
+api = application = falcon.API(middleware=[CorsMiddleware()])
 
 task_collection = tasks.tasks.Collection()
 
